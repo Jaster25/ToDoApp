69e2a1b0a5d9fe3a176acac73fdfe0427503c658
Jaster25 <js_328@naver.com>
2019-12-29 02:57:05 +0900

    2019-12-28

---
+++ 2.0/app.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".adder-form");
+const input = form.querySelector("input");
+
+
+
+function loadedTodos() {
+    
+}
+
+function init() {
+    const loadedTodos = loadToDos();
+    if(loadedTodos !== null)
+}
+
+init();
