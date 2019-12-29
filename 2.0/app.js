738a55cd7ec5db9e28da2049fedecb26ea8f1707
Jaster25 <js_328@naver.com>
2019-12-29 21:36:43 +0900

    2019-12-29

--- 2.0/app.js
+++ 2.0/app.js
@@ -11,26 +11,24 @@ const saveToDos = () => {
 
 const paintToDo = (TEXT, CHECK) => {
   const li = document.createElement("li");
-  const divBtnCheck = document.createElement("div");
-  const divBtnDelete = document.createElement("div");
-  const divSpan = document.createElement("div");
   const btnCheck = document.createElement("button");
   const btnDelete = document.createElement("button");
   const span = document.createElement("span");
 
-  divBtnCheck.className = "divBtn";
-  divBtnDelete.className = "divBtn";
-  divSpan.className = "divSpan";
+  span.className = "span";
+  span.innerText = TEXT;
   btnCheck.className = "btnCheck";
   btnDelete.className = "btnDelete";
-  span.innerText = TEXT;
+  if (CHECK) btnCheck.style.background = 'url("images/checked.png")';
+  else btnCheck.style.background = 'url("images/noChecked.png")';
+  btnCheck.style.backgroundSize = "100%,100%";
+  btnCheck.addEventListener("click", handleCheck);
+  btnDelete.addEventListener("click", handleDelete);
 
-  divBtnCheck.appendChild(btnCheck);
-  divSpan.appendChild(span);
-  divBtnDelete.appendChild(btnDelete);
-  li.appendChild(divBtnCheck);
-  li.appendChild(divSpan);
-  li.appendChild(divBtnDelete);
+  li.id = toDos.length + 1;
+  li.appendChild(btnCheck);
+  li.appendChild(span);
+  li.appendChild(btnDelete);
 
   const toDoObj = {
     text: TEXT,
@@ -38,7 +36,7 @@ const paintToDo = (TEXT, CHECK) => {
     id: toDos.length + 1
   };
 
-  list.insertBefore(li, list.childNodes[0]);
+  list.appendChild(li);
   toDos.push(toDoObj);
 };
 
@@ -60,6 +58,38 @@ const handleSubmit = event => {
     saveToDos();
   }
   input.value = "";
+};
+
+const handleDelete = event => {
+  event.preventDefault();
+  const li = event.target.parentNode;
+  list.removeChild(li);
+
+  const cleanedToDos = toDos.filter(toDo => {
+    return parseInt(li.id) !== toDo.id;
+  });
+  toDos = cleanedToDos;
+  for (let i = parseInt(li.id) - 1; i < toDos.length; ++i) {
+    toDos[i].id--;
+    list.childNodes[i].id--;
+  }
+
+  saveToDos();
+};
+
+const handleCheck = event => {
+  event.preventDefault();
+  const li = event.target.parentNode;
+  const btnCheck = li.querySelector(".btnCheck");
+
+  const newCheck = !toDos[parseInt(li.id) - 1].check;
+  toDos[parseInt(li.id) - 1].check = newCheck;
+
+  if (newCheck) btnCheck.style.background = 'url("images/checked.png")';
+  else btnCheck.style.background = 'url("images/noChecked.png")';
+  btnCheck.style.backgroundSize = "100%,100%";
+
+  saveToDos();
 };
 
 function init() {
